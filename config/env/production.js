fde6c1a6609a51295d003a179449d2e71fa555b0
michael <gershkom@gmail.com>
2014-11-28 23:05:13 +0200

    DB connection for production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/productcatalog',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://mgershko:11091973@dogen.mongohq.com:10042/productCatalog',
 	assets: {
 		lib: {
 			css: [
